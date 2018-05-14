050b763fa7541a902a1e8a6afed0648b8367f55e
palewire <ben.welsh@gmail.com>
2018-05-13 17:56:10 -0700

    Sample admin

--- example/blog/admin.py
+++ example/blog/admin.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from blog import models
 
-# Register your models here.
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("headline", "publication_date", "is_published")
+    date_hierarchy = "publication_date"
+    search = ("headline", "slug")
