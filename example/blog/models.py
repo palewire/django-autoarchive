3838a4f240599893a772ca48f4bfaa3bd575a2a8
palewire <ben.welsh@gmail.com>
2018-05-13 17:52:01 -0700

    Made a simple blog post model as an example

---
+++ example/blog/models.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Post(models.Model):
+    """
+    A blog post.
+    """
+    headline = models.CharField(max_length=500)
+    slug = models.SlugField(unique=True)
+    publication_date = models.DateField()
+    is_published = models.BooleanField(default=False)
+    body = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ("-publication_date",)
+        get_latest_by = "publication_date"
+
+    def __str__(self):
+        return self.headline
+
+    def get_absolute_url(self):
+        return '/post/{}/'.format(self.slug)
